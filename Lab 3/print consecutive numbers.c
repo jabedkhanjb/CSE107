b490ac712fec1f4bb127961c1d8345538bca6f10
jabedkhanjb <mahfuzislam1410@gmail.com>
2021-09-23 01:15:00 +0600

    Lab 3 added through git terminal command

---
+++ Lab 3/print consecutive numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    printf("***********************************\n");
+    int i, end;
+    printf("Print numbers from 1 to : ");
+    scanf("%d", &end);
+    i=1;
+    while(i<=end)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+    printf("\n\n****************jabedkhanjb******************\n");
+    return 0;
+}
