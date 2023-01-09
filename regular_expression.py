c202bc73a583d8803866e32da322ce9773e2c32d
Jabed Khan <86038917+jabedkhanjb@users.noreply.github.com>
2023-01-09 23:30:17 +0600

    Update regular_expression.py

--- regular_expression.py
+++ regular_expression.py
@@ -7,3 +7,18 @@ if match:
     # name = f"{first} {last}"
     name = match.group(2) + " " + match.group(1)
 print(f"Hello, {name}".title())
+import re 
+
+email = input("Please, insert your email : ")
+if re.search(r"^\w|\s+@\w+\.edu$", email):
+    # backslash w in this case represents a "word character"
+    # which is commonly known as a alphanumeric symbol or the underscore
+    print("This email is Valid")
+else:
+    print("This email is Invalid")
+    
+    
+#   ^   means "start matching at the beginning of the string"
+#   w   means "any word character"
+#   +   means "one or more"
+
